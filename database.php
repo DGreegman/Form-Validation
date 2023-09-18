31cd26b7c8beac0ef8da549e0a970a4446ac0343
Fredrick Peter <tamdevelopers@gmail.com>
2023-09-18 22:26:51 +0100

    database usage

---
+++ database.php
@@ -0,0 +1,11 @@
+<?php
+
+// for the database config.
+
+// -> Create a new project directory
+// -> install the package via composer
+// -> 
+
+
+autoloader_start();
+// then reload page in the browser, and your database project will be created automatically for usage
